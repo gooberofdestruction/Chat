7125933a2e996c5c849b24a0d4ffb254517a3e80
unknown <Mike@.(none)>
2011-04-07 20:36:49 -0700

    Chat

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      redirect_to messages_path, :notice => "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid login/password combination"
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    remove_user
+    reset_session
+    redirect_to messages_path, :notice => "You successfully logged out"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '6a27b4e4145bd04a530d6977566016ec47add1e5a4c3828c01a9fdc9b804e15ef973970aa8e0a4f25842ccdbcc79d9708e54de19af205e90a811a43d82f714be'
